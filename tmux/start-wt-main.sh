65bcd6acb8884f4efb578eb3e1d87e4917d72d6b
qrutyy <qrutyq@gmail.com>
2026-04-10 10:44:45 +0300

    feat: add default working configs

---
+++ tmux/start-wt-main.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+SESSION="w-main"
+
+tmux kill-session -t $SESSION 2>/dev/null
+tmux new-session -d -s $SESSION -n main
+
+# Split first window horizontally
+tmux split-window -h -t $SESSION:main
+
+# Split first pane vertically
+tmux split-window -v -t $SESSION:main.1
+
+# 0.0 - left-up
+tmux send-keys -t $SESSION:main.1 "cd engine && gs" C-m
+
+# 0.1 - left-down
+tmux send-keys -t $SESSION:main.2 "sudo su" C-m
+tmux send-keys -t $SESSION:main.2 "dmesg -C" C-m
+tmux send-keys -t $SESSION:main.2 "dmesg -w" C-m
+
+# 0.2 right
+tmux send-keys -t $SESSION:main.3 "sudo su" C-m
+tmux send-keys -t $SESSION:main.3 "source ~/.bash_profile" C-m
+
+# 1. - second window
+tmux new-window -d -t $SESSION -n nvim
+tmux send-keys -t $SESSION:nvim "cd engine && nvim" C-m
+
+tmux select-window -t $SESSION:main
+tmux select-pane -t $SESSION:main.1
+
+tmux attach-session -t $SESSION:main
